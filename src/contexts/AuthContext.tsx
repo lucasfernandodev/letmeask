26f893958ee2181768dba99a2d65838e25a26ce7
Lucas Fernando <lucasfernando.dev@gmail.com>
2021-06-23 15:22:23 -0300

    feat: regras ao bd, criação de salas

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -20,7 +20,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
   const [user, setUser] = useState<User>();
 
   useEffect(() => {
-    const unsubcribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         const { displayName, photoURL, uid } = user;
 
@@ -37,7 +37,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
     });
 
     return () => {
-      unsubcribe();
+      unsubscribe();
     };
   }, []);
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,40 +1,66 @@
-import {Link} from 'react-router-dom';
-import '../styles/auth.scss';
+import { Link, useHistory } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import "../styles/auth.scss";
+import ilustationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { database } from "../services/firebase";
+import { useAuth } from "../hooks/useAuth";
 
-import ilustationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import {Button} from '../components/Button';
-import { useAuth } from '../hooks/useAuth';
+export function NewRoom() {
 
-export function NewRoom (){
+  const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState("");
+  const history = useHistory();
 
-    const {user} = useAuth();
 
-    return (
-        <div id="page-auth">
-        <aside>
-            <img src={ilustationImg} alt="Ilustração snbolizando perguntas e respostas" />
-            <strong>Crie salas de Q&amp;A ao-vivo</strong>
-            <p>Tire as duvidas da sua audiencia em tempo real.</p>
-        </aside>
-        <main>
-            <div className="main-content">
-                <img src={logoImg} alt="Letmeask" />
-                <h2>Criar uma nova sala</h2>
-                <form>
-                    <input 
-                        type="text" 
-                        placeholder="Nome da sala"
-                    />
-                    <Button type="submit">
-                        Criar sala
-                    </Button>
-                </form>
-                <p>
-                    Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
-                </p>
-            </div>
-        </main>
+  async function handleCreateRoom(e: FormEvent) {
+    e.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/Rooms/${firebaseRoom.key}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={ilustationImg}
+          alt="Ilustração sinbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as duvidas da sua audiencia em tempo real.</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(event) => {
+                setNewRoom(event.target.value);
+              }}
+              value={newRoom}
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
+        </div>
+      </main>
     </div>
-    )
+  );
 }
